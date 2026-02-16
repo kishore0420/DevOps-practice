680f04bc6afbbdee858942922b8da7dd5579d780
kishore0420 <51207213+kishore0420@users.noreply.github.com>
2026-02-16 18:53:54 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "ubuntu" {
-  key_name      = "awspractice"
-  ami           = "ami-0b6c6ebed2801a5cb"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-00ed53140cf2eb31d"
-  tags = {
-    Name = "myinstance"
-  }
-}
